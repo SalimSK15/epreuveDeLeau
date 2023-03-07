95046a4cc9f4f65f15713da9523d0af26fd12320
salim <salimdu15@outlook.fr>
2023-03-07 05:04:22 +0100

    exo00 Combinisons de 3 chiffres

--- eau00.js
+++ eau00.js
@@ -3,23 +3,19 @@ function affichageValeur(tabValeurs){
     var tabValeursFinal = "";
     for (var i = 0; i < tabValeurs.length; i++)
         tabValeursFinal = tabValeursFinal + tabValeurs[i];
-    tabValeursFinal[tabValeursFinal.length-1] = "";
     
     return tabValeursFinal;     
-        //console.log(tabValeurs[i]);
+       
 }
 // gestion d'ereurs
 
 // le parsing
-var valeur = "";
 var tabValeurs = [];
 //la resolution
 for (var i = 0; i <= 7; i++)
     for(var j = i+1; j <= 8; j++ )
         for(var k = j+1; k <=9; k++){
             tabValeurs.push(i+""+j+""+k+",");
-            //if(i == 7 && k == 9)
-             //   tabValeurs.push(i+""+j+""+k+"");
         }
 tabValeurs[tabValeurs.length-1]="789";
 
